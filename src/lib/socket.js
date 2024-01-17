8edad77b2bdc20c903fea5aa8328fae87fb83980
PabloSA57 <camusps577@gmail.com>
2024-01-17 20:51:32 -0300

    prueba mp11

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,3 +1,6 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:3001", { autoConnect: false, auth: {} });
+const socket = io(process.env.NEXT_PUBLIC_API_URL, {
+  autoConnect: false,
+  auth: {},
+});
 export default socket;
